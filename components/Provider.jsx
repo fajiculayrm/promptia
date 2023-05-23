de189bebe28f35d4d7f29d8a32f864f8afe85ef1
Raymond <raymondfajiculay.dev@gmail.com>
2023-05-23 10:43:37 +0800

    finish

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+'use client';
 
 import { SessionProvider } from "next-auth/react";
 
-const Provider = () => {
-  return <div>Provider</div>;
-};
+const Provider = ({ children, session }) => (
+  <SessionProvider session={session}>
+    {children}
+  </SessionProvider>
+)
 
 export default Provider;
